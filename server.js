4a7158c30ad6b7165f69668c46bedad6e1bdf9ab
Saransh1301 <saranshsingh1301@gmail.com>
2026-05-14 16:00:24 +0530

    added whatsapp outgoing message logs

--- server.js
+++ server.js
@@ -635,6 +635,8 @@ async function sendWhatsAppReply(to, message) {
     // ✅ FIX 1: @lid aur special chars hata ke sirf numbers rakho
     const phone = String(to || "8750285420").split("@")[0].replace(/[^0-9]/g, "").replace(/^91/, "");
     console.log("[WHATSAPP SENDING TO]", phone);
+    console.log("[WHATSAPP Message ]", message);
+  
 
     const resp = await fetch(WA_API_URL, {
       method: "POST",
