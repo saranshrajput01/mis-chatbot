eef531bdb052893d28197c4856812e7610820328
Saransh1301 <saranshsingh1301@gmail.com>
2026-05-14 17:22:51 +0530

    fix phone 10 digit

--- server.js
+++ server.js
@@ -632,7 +632,7 @@ async function sendWhatsAppReply(to, message) {
     const WA_API_KEY = "b53f573d12b6f76a4480d9e512cd711525e488308528207478";
     const WA_API_URL = "https://app.mis.work/api/v1/message/create";
 
-    const phone = String(to || "8750285420").split("@")[0].replace(/[^0-9]/g, "");
+    const phone = String(to).split("@")[0].replace(/[^0-9]/g, "").replace(/^91/, "");
     console.log("[WHATSAPP SENDING TO]", phone);
 
     message = String(message).slice(0, 900);
